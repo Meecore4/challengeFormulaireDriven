e3b3d73ad1d96d97da6f0b7f827d4172414f03e7
Meecore4 <[alicia.critelli4@gmail.com](mailto:alicia.critelli4@gmail.com)>
2023-01-23 15:00:36 +0100

    2nd commit

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -5,12 +5,14 @@ export class Order {
   firstname: string;
   lastname: string;
   email: string;
+  password: string;
 
 
-  constructor(firstname: string, lastname: string, email: string) {
+  constructor(firstname: string, lastname: string, email: string, password:string) {
       this.firstname = firstname;
       this.lastname = lastname;
       this.email = email;
+      this.password = password;
       
 // OU 
 
@@ -32,7 +34,7 @@ export class Order {
 export class SignUpComponent {
 
 
-  model: Order = new Order('', '', '');
+  model: Order = new Order('', '', '', '');
   constructor(){}
 
 
